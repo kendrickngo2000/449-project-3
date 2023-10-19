095d5a5e9f36a5cf3b8628bd1e05deaf91c1a587
webdevlex <webdeveloperlex@gmail.com>
2023-10-18 17:51:01 -0700

    configured replications

--- api/bin/init.sh
+++ api/bin/init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sqlite3 ./var/enrollmentDatabase.db < ./share/enrollmentDatabase.sql
-sqlite3 ./var/authDatabase.db < ./share/authDatabase.sql
+sqlite3 ./var/primary/fuse/enrollmentDatabase.db < ./share/enrollmentDatabase.sql
+sqlite3 ./var/primary/fuse/authDatabase.db < ./share/authDatabase.sql
